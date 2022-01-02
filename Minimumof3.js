3f909cf19e127583ebed98575e3ad24fcde36b92
Lohithkumaras <89640966+Lohithkumaras@users.noreply.github.com>
2022-01-02 11:53:51 +0530

    Create Minimumof3.js

---
+++ Minimumof3.js
@@ -0,0 +1,16 @@
+alert("Enter 3 numbers");
+var a = prompt("Enter number 1");
+var b = prompt("Enter number 2");
+var c = prompt("Enter number 3");
+if(a<b)
+{
+if(a<c)
+ alert(a +" is Minimum number");
+}
+else 
+{
+  if(b<c)
+  alert(b +" is Minimum number");
+   else
+   alert(c +" is Minimum number");
+}
