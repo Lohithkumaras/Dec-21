0b9673637948826d79916f1f5d16af9021ec6c16
Lohithkumaras <89640966+Lohithkumaras@users.noreply.github.com>
2022-01-02 11:56:10 +0530

    Create Vowel.js

---
+++ Vowel.js
@@ -0,0 +1,5 @@
+var letter = prompt("Enter the letter to check wether it's Vowel or not");
+if(letter=="A"||letter=="E"||letter=="I"||letter=="O"||letter=="U"||letter=="a"||letter=="e"||letter=="i"|letter=="o"||letter=="u")
+alert(letter +" is Vowel");
+else
+alert(letter +" is not a Vowel");
